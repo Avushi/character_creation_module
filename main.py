8178461ba575709c1a129d15f240344e2bf5cf3e
Евгений Маркин <markinevgeniy97@gmail.com>
2022-11-20 22:04:55 +0300

    Работа над исправлением файла 123 из дз в директории character...Знаю, что так не правильно))

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -92,4 +92,3 @@ def main() -> None:
     print(start_training(char_name, char_class))
 
 main()
- 
